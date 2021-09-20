092cce12b7037a27d2b815c0ca3100100da4b29a
SoftProDev <softprodev15@gmail.com>
2021-09-20 13:57:02 +0100

    Cleaned up puppet challenge.

--- tests/test_puppet_challenge.py
+++ tests/test_puppet_challenge.py
@@ -13,29 +13,7 @@ ATTACKER_INITAL_TOKEN_BALANCE = Web3.toWei("100", "ether")
 
 def before():
     # setup scenario
-    deployer = accounts[0]
-    attacker = accounts[1]
-    deployer.transfer("", Web3.toWei("5", "ether"))
-    deployer.transfer("", Web3.toWei("5", "ether"))
-    deployer.transfer("", Web3.toWei("5", "ether"))
-
-    # Deploy the oracle and setup the trusted sources with initial prices
-    oracle_address = TrustfulOracleInitializer.deploy(
-        sources,
-        ["DVNFT", "DVNFT", "DVNFT"],
-        [INITIAL_NFT_PRICE, INITIAL_NFT_PRICE, INITIAL_NFT_PRICE],
-        {"from": deployer},
-    ).oracle()
-    oracle = Contract.from_abi("TrustfulOracle", oracle_address, TrustfulOracle.abi)
-
-    # Deploy the exchange and get the associated ERC721 token
-    exchange = Exchange.deploy(
-        oracle.address, {"from": deployer, "value": EXCHANGE_INITIAL_ETH_BALANCE}
-    )
-    token = Contract.from_abi("DamnValuableNFT", exchange.token(), DamnValuableNFT.abi)
-
-    global initial_attacker_balance
-    initial_attacker_balance = attacker.balance()
+    # HOW TO LOAD IN UNISWAP CONTRACTS?
 
 
 def run_exploit():
@@ -45,8 +23,7 @@ def run_exploit():
 
 
 def after():
-    # Confirm exchange lost all ETH
-    assert Exchange[0].balance() == 0
+
 
 
 def test_puppet_challenge():
